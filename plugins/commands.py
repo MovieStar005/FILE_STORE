02382c58c9a71e96f9b26947ad0ce7580dae090a
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2024-10-30 01:20:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,6 +22,7 @@ import base64
 from urllib.parse import quote_plus
 from TechVJ.utils.file_properties import get_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
+from pyrogram.types import Message
 
 BATCH_FILES = {}
 
