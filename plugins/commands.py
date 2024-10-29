c1d0ae47657d2ca8b9cae73f5fb2aa5945243b03
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2024-10-30 02:51:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,8 +22,20 @@ import base64
 from urllib.parse import quote_plus
 from TechVJ.utils.file_properties import get_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
+from pyrogram.types import CallbackQuery
 
 BATCH_FILES = {}
+async def is_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
+        except Exception as e:
+            pass
+    return btn
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
